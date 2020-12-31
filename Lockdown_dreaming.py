344ae7ab565e282ed0794ee1554dcc9ee35b6d9e
Shohid <68284522+Mshohid@users.noreply.github.com>
2020-12-31 23:04:45 +0000

    Add files via upload

---
+++ Lockdown_dreaming.py
@@ -0,0 +1,17 @@
+responses = {}
+
+poll = True 
+
+while poll:
+    name = input("Please enter your name ")
+    question = input("If you could travel anywhere, where would you want to go? ")
+
+    responses[name] = question
+
+    repeat = input("Would anyone else want to have a go? 'yes/no ")
+    if repeat == "no":
+        poll = False
+
+print("\n ---Results---")
+for name, response in responses.items():
+    print(name.title() + " would like to go to " + response.title() + ".")
