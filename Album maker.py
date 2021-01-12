20499deb590364d172af4e93ea2f2ab2040a8beb
Shohid <68284522+Mshohid@users.noreply.github.com>
2021-01-12 22:12:58 +0000

    Add files via upload

---
+++ Album maker.py
@@ -0,0 +1,8 @@
+def make_album(Artist, Album, Number):
+    person = {Artist : 'Artist Name', Album : 'Album Name', Number : 'Number Of Tracks'}
+    return person 
+
+musica = make_album('Eminem', 'Kamikaze', '12')
+print(musica)
+
+
