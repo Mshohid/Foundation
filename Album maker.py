77fdcc33c11cb554f7012542b4ddb3b3484ae70b
Shohid <68284522+Mshohid@users.noreply.github.com>
2021-01-12 22:15:44 +0000

    Update Album maker.py

--- Album maker.py
+++ Album maker.py
@@ -2,7 +2,7 @@ def make_album(Artist, Album, Number):
     person = {Artist : 'Artist Name', Album : 'Album Name', Number : 'Number Of Tracks'}
     return person 
 
-musica = make_album('Eminem', 'Kamikaze', '12')
+musica = make_album('Michael Jackson', 'Thriller', '8')
 print(musica)
 
 
